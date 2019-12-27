1d0d8442fa8b46d584dfd9d6b4640545ef1d0856
lishikai <15638055197@163.com>
2019-12-27 20:04:00 +0800

    新增页面逻辑--手工录入页面+excel导入页面

---
+++ src/components/lishikai/accidentManager.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "accidentManager"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/lishikai/fileupload.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "fileupload"
+    }
+</script>
+
+<style scoped>
+
+</style>
